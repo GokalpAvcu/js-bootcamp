31b9759f2bfe7850650641cf283ff55ddad3133e
gökalp <avcu219@gmail.com>
2021-10-21 15:59:52 +0300

    add js

--- src/oop/index.js
+++ src/oop/index.js
@@ -1,11 +1,12 @@
 class Customer{
     constructor(id,customerNumber){
-
+        this.id = id;
+        this.customerNumber = customerNumber;
     }
 }
 
 let customer = new Customer(1, "12345");
 //prototyping
 customer.name = "Gökalp Avcu"
-console.log(customer.id)
+console.log(customer.name)
 
