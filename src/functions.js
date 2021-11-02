865b3476597ca9831fe7a81efc04c44436beeee5
gökalp <avcu219@gmail.com>
2021-11-02 16:20:15 +0300

    add js

--- src/functions.js
+++ src/functions.js
@@ -8,10 +8,25 @@ addToCart("Karpuz")
  let sayHello = ()=>{   // sayHello adında bir değişken tanımladım
      console.log("Hello World")
  }
-
  sayHello()
 
  let sayHello2 = function () {
      console.log("Hello World 2")
  }
  sayHello2();
+
+ function addToCart2(productName, quantity,unitPrice) {
+     
+ }
+
+ addToCart2("Elma",5,10)
+ addToCart2("Armut",2,20)
+ addToCart2("Limon",3,15)
+
+ let product1 = {productName:"Elma", unitPrice:10, quantity:5}
+
+ function addToCart3(product) {
+     
+ }
+
+ addToCart3(product1)
