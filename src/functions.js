e792b8c8c310d85f70da6a2de39a7a105d86d95e
gökalp <avcu219@gmail.com>
2021-11-02 15:52:23 +0300

    add js

--- src/functions.js
+++ src/functions.js
@@ -5,4 +5,13 @@ addToCart()
 addToCart("Yumurta")
 addToCart("Karpuz")
 
- 
+ let sayHello = ()=>{   // sayHello adında bir değişken tanımladım
+     console.log("Hello World")
+ }
+
+ sayHello()
+
+ let sayHello2 = function () {
+     console.log("Hello World 2")
+ }
+ sayHello2();
