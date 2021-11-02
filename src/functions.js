e3a7c8621e4b063fd7774426404c05d196a775b4
gökalp <avcu219@gmail.com>
2021-11-02 21:54:23 +0300

    add js

--- src/functions.js
+++ src/functions.js
@@ -88,3 +88,9 @@ let numbers = [30,10,500,600,120]
  //console.log(icAnadolu.name)
  //console.log(icAnadolu.population)
  console.log(icAnadoluSehirleri)
+
+
+
+ let product5 = {productName:"Elma", unitPrice:10, quantity:5}
+
+ console.log(product5)
